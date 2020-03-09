d7487d7ba708ff71a0520da60ae81bddaba5e852
iseekTo <1214046231@qq.com>
2020-03-09 20:09:15 +0800

    fix: ngnix refesh 404

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './router/index';
 
 
@@ -8,9 +8,6 @@ declare const module: any;
 
 const render = () => {
     ReactDOM.render(
-        // <HashRouter>
-        //     <App />
-        // </HashRouter>,
         <BrowserRouter>
             <App />
         </BrowserRouter>,
