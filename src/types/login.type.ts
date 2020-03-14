113319478aa034158275092a20e18fad0d3eec78
iseekTo <1214046231@qq.com>
2020-03-14 16:07:37 +0800

    Update login.type.ts

--- src/types/login.type.ts
+++ src/types/login.type.ts
@@ -1,7 +1,21 @@
 
-//-- login --
+// -- login --
+
+namespace Login {
+    export interface web {
+        username: string
+        password: string
+    }
+
+    export interface other {
+        [x: string]: string
+    }
+}
+
+
 export interface i_loginType {
     // path: string
     Component: any
     [key: string]: any
 }
+
